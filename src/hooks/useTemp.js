c8b9ac379567933856f3b875b88f6e26e1840587
Aneeshkumar2001 <aneeshkumarakm01@gmail.com>
2024-09-02 11:47:08 +0530

    Weather app

---
+++ src/hooks/useTemp.js
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import AppContext from "../provider/appContext";
+
+function useTemp(temp, toFixed = 0) {
+  const {
+    app: { unit },
+  } = useContext(AppContext);
+
+  if (unit.toLowerCase() === "f") {
+    return (((temp - 273.15)* 9) / 5 + 32).toFixed(toFixed);
+  }
+
+  return parseFloat(temp - 273.15).toFixed(toFixed);
+}
+
+export default useTemp;
